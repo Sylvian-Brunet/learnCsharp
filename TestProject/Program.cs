1d9c1e83c38bb6466437ecb95964a78549fea06a
Sylvian-Brunet <sylvian84@hotmail.fr>
2024-01-18 22:12:17 +0100

    training - modules - csharp-if-elseif-else - 4

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,29 +1,21 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
-    if ((roll1 == roll2) && (roll2 == roll3)) {
-        Console.WriteLine("You rolled triples! +6 bonus to total!");
-        total += 6;
-    } else {
-        Console.WriteLine("You rolled doubles! +2 bonus to total!");
-        total += 2;
+if(daysUntilExpiration <= 10) { // Règle 6
+    if(daysUntilExpiration == 0) { // Règle 5
+        Console.WriteLine("Your subscription as expired.");
+    } else if(daysUntilExpiration == 1) { // Règle 4
+        discountPercentage = 20;
+        Console.WriteLine("Your subscription expires within a day!");
+    } else if (daysUntilExpiration <= 5) { // Règle 3
+        discountPercentage = 10;
+        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} day(s)!");
+    } else { // Règle 2
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
     }
-}
 
-if (total >= 16) {
-    Console.WriteLine("You win a new car!");
-} else if (total >= 10) {
-    Console.WriteLine("You win a new laptop!");
-} else if (total == 7) {
-    Console.WriteLine("You win a trip for two!");
-} else {
-    Console.WriteLine("You win a kitten!");
+    if(discountPercentage > 0) {
+        Console.WriteLine($"Renew now and save {discountPercentage}%!");
+    }
 }
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03186c47aa8d21a79313a62ce4da164106b1f189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba57251bc359cda1bcf81d398a7e949f5fd7928")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
